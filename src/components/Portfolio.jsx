b7ba020f5c81da7c592a48119855a40b2f10e812
hasanali207 <hasanali142228@gmail.com>
2024-07-06 13:49:38 +0600

    added data

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { FaGithub, FaGlobe } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
-import portfolio1 from '../assets/img/port1.jpg';
-import portfolio2 from '../assets/img/port2.jpg';
-import portfolio3 from '../assets/img/port3.jpg';
+import portfolio1 from '../assets/img/port1.png';
+import portfolio2 from '../assets/img/port2.png';
+import portfolio3 from '../assets/img/port3.png';
 
 const Portfolio = () => {
   return (
@@ -12,215 +12,237 @@ const Portfolio = () => {
               <h2 className='text-4xl uppercase text-center font-semibold text-slate-600 my-10'>Recent Project</h2>
               
             </div>  
-    <div className=' max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-4 p-4 lg:p-0'>
+    <div className=' max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-4 p-4 lg:p-0'>
         {/* portfolio 1  */}
         <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full'>
-      <Link to="/some-path">  {/* Ensure to replace "/some-path" with the actual path */}
         <img
           src={portfolio1}
           alt='listing cover'
-          className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
+          className='w-full object-cover hover:scale-105 transition-scale duration-300'
         />
         <div className='p-3 flex flex-col gap-2 w-full'>
           <p className='truncate text-lg font-semibold text-slate-700'>
-            Web Development
+           Scholar Management System
           </p>
-          <div className='flex items-center gap-1'>
-            <p className='text-sm text-gray-600 truncate w-full'>
-              Dhaka, Bangladesh
-            </p>
-          </div>
-          <p className='text-sm text-gray-600 line-clamp-2'>
-            This is a description
+         
+          <p className='text-sm text-gray-600'>
+          Description: Scholarship Management System
+where student can search there suitable university and Scholarship also they can
+apply for the scholarship through our Scholarship Management System.
+In our website will be 3 type of user one is by default user and another one is
+admin and another one is moderator.
           </p>
-          <p className='text-slate-500 mt-2 font-semibold'>
-            {/* Add any additional information here */}
+
+          <details className="w-full border rounded-lg" open="">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:dark:ring-violet-600 cursor-pointer">Features
+        </summary>
+				<ul className="px-4 py-6 pt-0 ml-4 -mt-4 list-decimal dark:text-gray-600">
+          <li>User login, register, and logout          </li>  
+          <li>User role-based access</li>  
+          <li>Teacher panel  </li>  
+          <li>Admin panel</li>  
+          <li>Payment system </li>  
+          <li>Pagination functionality </li>  
+          <li> Feedback system</li>  
+        </ul>
+			</details>
+
+      <details className=" cursor-pointer w-full border rounded-lg" open="">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:dark:ring-violet-600">Techs I used
+        </summary>
+         <div className='flex flex-wrap space-x-2 px-2'>
+         <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            React
           </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Javascript
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Node.js
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Express.js  
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Mongodb
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Firebase
+          </p>
+         </div>
+			</details>
+
+
           <div className='text-slate-700 flex gap-4'>
             <div className='font-bold text-xs'>
               {/* Add any additional information here */}
             </div>
+            <div className='text-slate-700 flex gap-4'>
+            <div className='font-bold flex space-x-4'>
+
+            <a target='_blank' className=' border rounded-lg p-2 bg-slate-200 flex items-center space-x-3'  href="https://scholar-stream.web.app/"><button>Live </button><FaGlobe></FaGlobe></a>
+            <a className=' border rounded-lg p-2 bg-slate-200 flex items-center space-x-3'  href="https://github.com/hasanali207/scholar-stream"><button>Client </button><FaGithub></FaGithub></a>
+            <a className=' border rounded-lg p-2 bg-slate-200 flex items-center space-x-3'  href=""><button>Server </button><FaGithub></FaGithub></a>
+           
+            </div>
             <div className='font-bold text-xs'>
-              {/* Add any additional information here */}
+
             </div>
           </div>
+          </div>
         </div>
-      </Link>
+      
         </div>
-         {/* portfolio 2  */}
+        {/* portfolio 2  */}
         <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full'>
-      <Link to="/some-path">  {/* Ensure to replace "/some-path" with the actual path */}
         <img
           src={portfolio2}
           alt='listing cover'
-          className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
+          className='w-full object-cover hover:scale-105 transition-scale duration-300'
         />
         <div className='p-3 flex flex-col gap-2 w-full'>
           <p className='truncate text-lg font-semibold text-slate-700'>
-            Web Development
+           Library Management System
           </p>
-          <div className='flex items-center gap-1'>
-            <p className='text-sm text-gray-600 truncate w-full'>
-              Dhaka, Bangladesh
-            </p>
-          </div>
-          <p className='text-sm text-gray-600 line-clamp-2'>
-            This is a description
+         
+          <p className='text-sm text-gray-600'>
+          Description: This website that allows Librarian to add books of diffrent categories to their collection, and update book information. Bokks can be borrowed ro returend. The system keeps track it.
           </p>
-          <p className='text-slate-500 mt-2 font-semibold'>
-            {/* Add any additional information here */}
+
+          <details className="w-full border rounded-lg" open="">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:dark:ring-violet-600 cursor-pointer">Features
+        </summary>
+				<ul className="px-4 py-6 pt-0 ml-4 -mt-4 list-decimal dark:text-gray-600">
+          <li>User login, register, and logout          </li>  
+          <li>User role-based access</li>  
+          <li>Librarian panel  </li>  
+          <li>User panel</li>  
+          <li>Pagination functionality </li>  
+          <li> Feedback system</li>  
+        </ul>
+			</details>
+
+      <details className=" cursor-pointer w-full border rounded-lg" open="">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:dark:ring-violet-600">Techs I used
+        </summary>
+         <div className='flex flex-wrap space-x-2 px-2'>
+         <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            React
           </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Javascript
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Node.js
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Express.js  
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Mongodb
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Firebase
+          </p>
+         </div>
+			</details>
+
+
           <div className='text-slate-700 flex gap-4'>
             <div className='font-bold text-xs'>
               {/* Add any additional information here */}
             </div>
+            <div className='text-slate-700 flex gap-4'>
+            <div className='font-bold flex space-x-4'>
+
+            <a target='_blank' className=' border rounded-lg p-2 bg-slate-200 flex items-center space-x-3'  href="https://book-haven-six.vercel.app/"><button>Live </button><FaGlobe></FaGlobe></a>
+            <a className=' border rounded-lg p-2 bg-slate-200 flex items-center space-x-3'  href="https://github.com/hasanali207/book-library"><button>Client </button><FaGithub></FaGithub></a>
+            <a className=' border rounded-lg p-2 bg-slate-200 flex items-center space-x-3'  href=""><button>Server </button><FaGithub></FaGithub></a>
+           
+            </div>
+            <div className='font-bold text-xs'>
+
+            </div>
+          </div>
+          </div>
+        </div>
+      
+        </div>
+        <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full'>
+        <img
+          src={portfolio3}
+          alt='listing cover'
+          className='w-full object-cover hover:scale-105 transition-scale duration-300'
+        />
+        <div className='p-3 flex flex-col gap-2 w-full'>
+          <p className='truncate text-lg font-semibold text-slate-700'>
+           Art & Craft Store
+          </p>
+         
+          <p className='text-sm text-gray-600'>
+          Description: Art & Craft Store with a backend API using Node.js and Express and a frontend using React. The React app fetches the store description from the backend and displays it on the page. This example can be expanded with additional features such as product listings, user authentication, and more complex UI elements to create a fully functional store.
+          </p>
+
+          <details className="w-full border rounded-lg" open="">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:dark:ring-violet-600 cursor-pointer">Features
+        </summary>
+				<ul className="px-4 py-6 pt-0 ml-4 -mt-4 list-decimal dark:text-gray-600">
+          <li>User login, register, and logout          </li>  
+          <li>User role-based access</li>  
+          <li>Admin panel  </li>  
+          <li>User panel</li>  
+          <li>Pagination functionality </li>  
+          <li> Feedback system</li>  
+        </ul>
+			</details>
+
+      <details className=" cursor-pointer w-full border rounded-lg" open="">
+				<summary className="px-4 py-6 focus:outline-none focus-visible:dark:ring-violet-600">Techs I used
+        </summary>
+         <div className='flex flex-wrap space-x-2 px-2'>
+         <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            React
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Javascript
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Node.js
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Express.js  
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Mongodb
+          </p>
+          <p className="tl-duration bg-gray-200 rounded-full px-3 py-2 text-sm uppercase font-medium inline-block mb-4">
+            Firebase
+          </p>
+         </div>
+			</details>
+
+
+          <div className='text-slate-700 flex gap-4'>
             <div className='font-bold text-xs'>
               {/* Add any additional information here */}
             </div>
-          </div>
-        </div>
-      </Link>
-        </div>
-         {/* portfolio 3  */}
-        <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full'>
-      <Link to="/some-path">  {/* Ensure to replace "/some-path" with the actual path */}
-        <img
-          src={portfolio3}
-          alt='listing cover'
-          className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
-        />
-        <div className='p-3 flex flex-col gap-2 w-full'>
-          <p className='truncate text-lg font-semibold text-slate-700'>
-            Web Development
-          </p>
-          <div className='flex items-center gap-1'>
-            <p className='text-sm text-gray-600 truncate w-full'>
-              Dhaka, Bangladesh
-            </p>
-          </div>
-          <p className='text-sm text-gray-600 line-clamp-2'>
-            This is a description
-          </p>
-          <p className='text-slate-500 mt-2 font-semibold'>
-            
-          </p>
-          <div className='text-slate-700 flex gap-4'>
-            <div className='font-bold text-xl flex space-x-4'>
-             <FaGlobe></FaGlobe>
-             <FaGithub></FaGithub>
+            <div className='text-slate-700 flex gap-4'>
+            <div className='font-bold flex space-x-4'>
+
+            <a target='_blank' className=' border rounded-lg p-2 bg-slate-200 flex items-center space-x-3'  href="https://artisan-avenue-fe34e.web.app/"><button>Live </button><FaGlobe></FaGlobe></a>
+            <a className=' border rounded-lg p-2 bg-slate-200 flex items-center space-x-3'  href="https://github.com/hasanali207/Artisan-Avenue"><button>Client </button><FaGithub></FaGithub></a>
+            <a className=' border rounded-lg p-2 bg-slate-200 flex items-center space-x-3'  href=""><button>Server </button><FaGithub></FaGithub></a>
+           
             </div>
             <div className='font-bold text-xs'>
+
+            </div>
+          </div>
+          </div>
+        </div>
+      
+        </div>
                 
-            </div>
-          </div>
-        </div>
-      </Link>
-        </div>
-         {/* portfolio 4 */}
-        <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full'>
-      <Link to="/some-path">  {/* Ensure to replace "/some-path" with the actual path */}
-        <img
-          src={portfolio3}
-          alt='listing cover'
-          className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
-        />
-        <div className='p-3 flex flex-col gap-2 w-full'>
-          <p className='truncate text-lg font-semibold text-slate-700'>
-            Web Development
-          </p>
-          <div className='flex items-center gap-1'>
-            <p className='text-sm text-gray-600 truncate w-full'>
-              Dhaka, Bangladesh
-            </p>
-          </div>
-          <p className='text-sm text-gray-600 line-clamp-2'>
-            This is a description
-          </p>
-          <p className='text-slate-500 mt-2 font-semibold'>
-            
-          </p>
-          <div className='text-slate-700 flex gap-4'>
-            <div className='font-bold text-xl flex space-x-4'>
-             <FaGlobe></FaGlobe>
-             <FaGithub></FaGithub>
-            </div>
-            <div className='font-bold text-xs'>
-                
-            </div>
-          </div>
-        </div>
-      </Link>
-        </div>
-         {/* portfolio 5 */}
-        <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full'>
-      <Link to="/some-path">  {/* Ensure to replace "/some-path" with the actual path */}
-        <img
-          src={portfolio3}
-          alt='listing cover'
-          className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
-        />
-        <div className='p-3 flex flex-col gap-2 w-full'>
-          <p className='truncate text-lg font-semibold text-slate-700'>
-            Web Development
-          </p>
-          <div className='flex items-center gap-1'>
-            <p className='text-sm text-gray-600 truncate w-full'>
-              Dhaka, Bangladesh
-            </p>
-          </div>
-          <p className='text-sm text-gray-600 line-clamp-2'>
-            This is a description
-          </p>
-          <p className='text-slate-500 mt-2 font-semibold'>
-            
-          </p>
-          <div className='text-slate-700 flex gap-4'>
-            <div className='font-bold text-xl flex space-x-4'>
-             <FaGlobe></FaGlobe>
-             <FaGithub></FaGithub>
-            </div>
-            <div className='font-bold text-xs'>
-                
-            </div>
-          </div>
-        </div>
-      </Link>
-        </div>
-         {/* portfolio 6 */}
-        <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full'>
-      <Link to="/some-path">  {/* Ensure to replace "/some-path" with the actual path */}
-        <img
-          src={portfolio3}
-          alt='listing cover'
-          className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300'
-        />
-        <div className='p-3 flex flex-col gap-2 w-full'>
-          <p className='truncate text-lg font-semibold text-slate-700'>
-            Web Development
-          </p>
-          <div className='flex items-center gap-1'>
-            <p className='text-sm text-gray-600 truncate w-full'>
-              Dhaka, Bangladesh
-            </p>
-          </div>
-          <p className='text-sm text-gray-600 line-clamp-2'>
-            This is a description
-          </p>
-          <p className='text-slate-500 mt-2 font-semibold'>
-            
-          </p>
-          <div className='text-slate-700 flex gap-4'>
-            <div className='font-bold text-xl flex space-x-4'>
-             <FaGlobe></FaGlobe>
-             <FaGithub></FaGithub>
-            </div>
-            <div className='font-bold text-xs'>
-                
-            </div>
-          </div>
-        </div>
-      </Link>
-        </div>
     </div>
    </>
   );
