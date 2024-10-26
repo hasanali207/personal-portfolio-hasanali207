cd98a22190a204bf2a084b7353ef572bf600a168
hasanali207 <hasanali142228@gmail.com>
2024-10-26 23:23:54 +0600

    edited skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import img9 from '../assets/Bootsrap.png';
+import img1 from '../assets/html.png';
 import img2 from '../assets/css.png';
-import img3 from '../assets/express.png';
-import img4 from '../assets/firebase.png';
-import img5 from '../assets/HTML.png';
-import img6 from '../assets/JavaScript.png';
-import img7 from '../assets/mongo.png';
-import img8 from '../assets/node.png';
-import img1 from '../assets/react.png';
-import img10 from '../assets/sass.png';
-import img11 from '../assets/tailwind.png';
+import img3 from '../assets/tailwind.png';
+import img4 from '../assets/bootstrap.png';
+import img5 from '../assets/react.png';
+import img6 from '../assets/node.png';
+import img7 from '../assets/react.png';
+import img8 from '../assets/react.png';
+import img9 from '../assets/react.png';
+import img10 from '../assets/react.png';
 
 
 export default function Skills() {
@@ -25,14 +23,43 @@ export default function Skills() {
 
       </div>
 
-      <div className="grid grid-cols-3 gap-4">
-        <div className="h-32 w-32 bg-cover rounded-full" style={{ backgroundImage: `url(${img1})` }}></div>
-        <div className="h-32 w-32 bg-cover rounded-full" style={{ backgroundImage: `url(${img2})` }}></div>
-        <div className="h-32 w-32 bg-cover rounded-full" style={{ backgroundImage: `url(${img3})` }}></div>
-        <div className="h-32 w-32 bg-cover rounded-full" style={{ backgroundImage: `url(${img4})` }}></div>
-        <div className="h-32 w-32 bg-cover rounded-full" style={{ backgroundImage: `url(${img5})` }}></div>
+      <div className="flex justify-center items-center">
+
+      {/* item1 */}
+      <div className="flex flex-col justify-center items-center skill-item">
         
+        <div className='icon-box'>
+          <img className="w-20 h-20" src={img7} alt="HTML" />
+          
+        </div>
+        <p className="text-lg mt-4 text-[#8750f7]">HTML</p>
+               
       </div>
+      <div className="flex flex-col justify-center items-center skill-item">
+        
+        <div className='icon-box'>
+          <img className="w-20 h-20" src={img6} alt="HTML" />
+          
+        </div>
+        <p className="text-lg mt-4 text-[#8750f7]">Node</p>
+               
+      </div>
+      <div className="flex flex-col justify-center items-center skill-item">
+        
+        <div className='icon-box'>
+          <img className="w-20 h-20" src={img4} alt="HTML" />
+          
+        </div>
+        <p className="text-lg mt-4 text-[#8750f7]">Bootstrap</p>
+               
+      </div>
+
+
+
+
+      </div>
+
+
 
     </div>
   )
